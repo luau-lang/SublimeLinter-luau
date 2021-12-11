d783dda62afd8e06b3f825b4078832a651038a2d
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2021-12-11 12:47:19 -0800

    Address review feedback

--- linter.py
+++ linter.py
@@ -1,17 +1,16 @@
 from SublimeLinter.lint import Linter, LintMatch, STREAM_STDERR
 
 class Luau(Linter):
-    cmd = 'luau-analyze --formatter=gnu ${file}'
-    regex = r'^.+:(?P<line>\d+)\.(?P<col>\d+)\-(?P<line_end>\d+)\.(?P<col_end>\d+): (?P<message>.+)'
+    cmd = 'luau-analyze --formatter=gnu ${file_on_disk}'
+    regex = r'^.+:(?P<line>\d+)\.(?P<col>\d+)\-(?P<end_line>\d+)\.(?P<end_col>\d+): (?P<code>\w+): (?P<message>.+)'
     defaults = {
         'selector': 'source.lua, source.luae'
     }
     error_stream = STREAM_STDERR
+    tempfile_suffix = '-'
 
     def split_match(self, match):
-        match, line, col, error, warning, msg, _ = super().split_match(match)
-        line_end = int(match.group("line_end")) - 1
-        col_end = int(match.group("col_end"))
-        error_type = 'error' if msg.startswith('SyntaxError') or msg.startswith('TypeError') else 'warning'
-
-        return LintMatch(match=match, line=line, col=col, end_line=line_end, end_col=col_end, error_type=error_type, message=msg)
+        error = super().split_match(match)
+        error['error_type'] = 'error' if error['code'].endswith('Error') else 'warning'
+        error['end_col'] += 1 # convert inclusive column end into exclusive
+        return error
