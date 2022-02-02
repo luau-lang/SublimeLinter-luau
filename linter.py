cc4fbce8258530ffca01783214291ff63712ed08
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2022-02-01 23:05:42 -0800

    Switch to stdin mode
    This allows us to produce feedback in realtime. This isn't fully compatible with require(),
    but this doesn't matter as much for now as require() support is still half-baked.
    
    Requires luau-analyze 0.512

--- linter.py
+++ linter.py
@@ -1,13 +1,12 @@
 from SublimeLinter.lint import Linter, LintMatch, STREAM_STDERR
 
 class Luau(Linter):
-    cmd = 'luau-analyze --formatter=gnu ${file_on_disk}'
-    regex = r'^.+:(?P<line>\d+)\.(?P<col>\d+)\-(?P<end_line>\d+)\.(?P<end_col>\d+): (?P<code>\w+): (?P<message>.+)'
+    cmd = 'luau-analyze --formatter=gnu -'
+    regex = r'^stdin:(?P<line>\d+)\.(?P<col>\d+)\-(?P<end_line>\d+)\.(?P<end_col>\d+): (?P<code>\w+): (?P<message>.+)'
     defaults = {
         'selector': 'source.lua, source.luae'
     }
     error_stream = STREAM_STDERR
-    tempfile_suffix = '-'
 
     def split_match(self, match):
         error = super().split_match(match)
